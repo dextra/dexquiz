13086f56229eaad985a5ea0333af92aea01e281d
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2020-09-16 17:36:00 -0300

    adding auth state

---
+++ src/DexQuiz.Client/Features/Authentication/AuthenticationState.cs
@@ -0,0 +1,18 @@
+﻿using BlazorState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DexQuiz.Client.Features.Authentication
+{
+    public partial class AuthenticationState : BaseState<AuthenticationState>
+    {
+        public string Token { get; private set; }
+        public  MyProperty { get; set; }
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
